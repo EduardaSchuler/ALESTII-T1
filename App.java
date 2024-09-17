8212f8e7c3c06ee9d2cfc1250bbe72a633adfa21
eduardaschuler <dudaschuler2011@gmail.com>
2024-09-17 19:44:35 -0300

    retorna os valores de todas as arvores

--- App.java
+++ App.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        MatrizArvore tree = new MatrizArvore("Casos de Teste\\casof30.txt");
+        MatrizArvore matriz;
+        
+        matriz =  new MatrizArvore("Casos de Teste\\casof30.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof60.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof90.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof120.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof150.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof180.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof250.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof300.txt");
+        matriz =  new MatrizArvore("Casos de Teste\\casof500.txt");
     }
 }
