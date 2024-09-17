5338d652aa181edd88ecfc452d69d649d9943cbb
eduardaschuler <dudaschuler2011@gmail.com>
2024-09-17 13:39:49 -0300

    percorre arvore e identifica raiz

--- App.java
+++ App.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class App{
-    public static void main(String args[]){
-        
+    public static void main(String[] args) {
+        ManipulaMatriz matriz = new ManipulaMatriz();
+        char[][] matrizArvore = Reader.readFile("casof30.txt");
+        for (int i = 0; i < matrizArvore.length; i++) {
+            System.out.println(matrizArvore[i]);
     }
-
-
-
+        System.out.println("raiz: " + matriz.acharValorRaiz());
+        System.out.println(matriz.encontrarCaminhoComMaiorSoma());
+    }
 }
